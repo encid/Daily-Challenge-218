b99ebe8bc46de7a09963778f9b4dfdbbb6f6d4fd
encid <ryancav@gmail.com>
2017-02-02 11:34:17 -0500

    a

--- Daily-Challenge-218/Program.cs
+++ Daily-Challenge-218/Program.cs
@@ -8,8 +8,38 @@ namespace DailyChallenge218
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine(Palindromic(71));
+
+            Console.ReadKey();
+        }
+
+        static string Palindromic(decimal num)
+        {
+            decimal originalNum = num;
+            int count = 0;
+
+            if (IsPalindrome(num.ToString()))
+                return string.Format("{0} gets palindromic after 0 steps: {0}", num);
+
+            while (true) {
+                count++;
+                num += GetReverse(num);
+                Console.WriteLine(num);
+                if (IsPalindrome(num.ToString()))
+                    return string.Format("{0} gets palindromic after {1} steps: {2}", originalNum, count, num);                
+            }
+        }
+
+        static bool IsPalindrome(string str)
+        {
+            return str == string.Concat( str.Reverse() );
+        }
+
+        static decimal GetReverse(decimal num)
+        {
+            return Convert.ToDecimal(string.Concat(num.ToString().Reverse()));
         }
     }
 }
